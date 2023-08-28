2ff7ce46ba23f670d75acdd5b84ef591c240c3b8
hannan_ssh <hannanhoney5000@gmail.com>
2023-08-27 22:57:53 -0300

    working on card

---
+++ frontend/src/components/cardVestibular/card.jsx
@@ -0,0 +1,14 @@
+import './card.css';
+
+const Card = () => {
+  return (
+    <div className="card-vest">
+      <div className="card-vest-text-part">
+        <h3>PAS1<br/> 2021 </h3>
+      </div>
+      <button className="button-card">INSCREVER</button>
+    </div>
+  );
+};
+
+export default Card;
--- frontend/src/pages/RegisterCourse/registercourse.jsx
+++ frontend/src/pages/RegisterCourse/registercourse.jsx
@@ -1,12 +1,34 @@
 import Navbar from "../../components/navbarUser/navbarUser";
-
+import Card from "../../components/cardVestibular/card";
+import './registercourse.css'
+import NavItems from "../../components/navitems/navitems";
 function RegisterCoursePage(){
 
 
     return(
   <div>
     <Navbar/>
-
+    <NavItems/>
+     <div className="main-register-course"> 
+     
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    
+    <Card/>
+     </div>
     hello
   </div>
 
